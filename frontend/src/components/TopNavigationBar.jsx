a74a1dceeb41085034977b63bbc561f491bcc750
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-15 09:51:47 -0600

    Add className to button. Add TODO comment

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+//TODO Check with mentor if there's another way of styling the button other than making a new style. Check the style in TopNavigationBar.scss
+
+import React, { useState } from 'react';
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = () => {
+
+  const [ favorited, setFavorited ] = useState(false);
+
+  const handleClick = () => setFavorited(!favorited);  
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <FavBadge />
+       <button className="fav-button" onClick={handleClick}>
+          <FavBadge selected={favorited}/>
+        </button>
+      
     </div>
   )
 }
