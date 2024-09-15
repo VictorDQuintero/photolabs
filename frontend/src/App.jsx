20f7c9ad64fdd0b6d679e328eb89cee7bf93c7db
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-14 22:41:56 -0600

    Add TopicList import and component, remove commented out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
 
-// import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
+import TopicList from 'components/TopicList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-
-  // const photosArr = [];
-
-  // for (let i = 0; i < 3; i++){
-  //   photosArr.push(sampleDataForPhotoListItem);    
-  // }
-
-  // const photo = photosArr.map((samplePhoto, index) => {
-  //   return (
-  //     <PhotoListItem 
-  //       key={index}
-  //       photo={samplePhoto}
-  //     />   
-  //   );
-  // });
 
   return (
     <div className="App">     
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
     <PhotoList/>       
       {/* {photo}       */}
+      {/* { Array.from(Array(3)).map((_, index) => <TopicListItem key={index}/>) } */}
+      <TopicList />
     </div>
   );
 };
