3f05c8d9ca9523fc59981f28f24fd79fb058f6ed
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-15 10:29:50 -0600

    Add mock data, Add helper function to parse data, Send parsed data as props to HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+
 import HomeRoute from 'routes/HomeRoute';
+
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
+
 import './App.scss';
+
+const prepareData = function (object) {
+    return Object.values(object);
+  }
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
+  const preparedPhotos = prepareData(photos);
+  const preparedTopics = prepareData(topics);
+
   return (
     <div className="App">     
-      <HomeRoute />
+      <HomeRoute photos={preparedPhotos} topics={preparedTopics}/>
     </div>
   );
 };
