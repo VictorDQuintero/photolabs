7cf61107c582c0cc9e7a4474e01ed19f8c0c96aa
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-15 09:59:53 -0600

    Add HomeRoute component. Remove commented out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-
-import PhotoList from './components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -10,12 +7,7 @@ const App = () => {
 
   return (
     <div className="App">     
-    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    {/* <PhotoList/>        */}
-      
-      {/* { Array.from(Array(3)).map((_, index) => <TopicListItem key={index}/>) } */}
-    {/* <TopicList /> */}
-    <TopNavigation />
+      <HomeRoute />
     </div>
   );
 };
