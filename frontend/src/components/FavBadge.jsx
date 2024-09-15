2abc2e948a04dc4a106de845b1c0e1849c3ee9d4
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-15 09:49:24 -0600

    Modify code to make badge toggle.Add TODO comment

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,14 +1,22 @@
+//TODO Figure out exactly what does the commented out code means
+
 import React from 'react';
 import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
-  return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
-    </div>
-  ) 
-};
+// const FavBadge = ({ isFavPhotoExist }) => {
+//   return (
+//     <div className='fav-badge'>
+//       <FavIcon displayAlert={!!isFavPhotoExist}/>
+//     </div>
+//   ) 
+// };
+
+const FavBadge = ({ selected }) => (
+  <div className='fav-badge'>
+    <FavIcon selected={selected} />
+  </div>
+);
 
 export default FavBadge;
