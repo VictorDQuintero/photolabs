c1e106172632b6bc18f29b758a713ec24dd488a2
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-14 23:12:04 -0600

    Commented out components, Add TopNavigation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PhotoList from './components/PhotoList';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -10,10 +11,11 @@ const App = () => {
   return (
     <div className="App">     
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <PhotoList/>       
-      {/* {photo}       */}
+    {/* <PhotoList/>        */}
+      
       {/* { Array.from(Array(3)).map((_, index) => <TopicListItem key={index}/>) } */}
-      <TopicList />
+    {/* <TopicList /> */}
+    <TopNavigation />
     </div>
   );
 };
