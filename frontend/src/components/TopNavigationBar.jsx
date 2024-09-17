3ebc01272d7f5b74202c030ebf3fbe3cdebd117f
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-17 16:54:39 -0600

    Add isFavPhotoExist variable

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,4 +1,5 @@
 //TODO Check with mentor if there's another way of styling the button other than making a new style. Check the style in TopNavigationBar.scss
+//TODO the favbdge does not need to be a button, at least not now
 
 import React, { useState } from 'react';
 import TopicList from './TopicList';
@@ -7,17 +8,15 @@ import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = (props) => {
 
-  const [ favorited, setFavorited ] = useState(false);
+const isFavPhotoExist = props.favoritePhotos.length;
 
-  const handleClick = () => setFavorited(!favorited);  
-
-  return (
+ return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics}/>
-       <button className="fav-button" onClick={handleClick}>
-          <FavBadge selected={favorited}/>
-        </button>
+      <TopicList topics={props.topics} />
+        {/* <button className="fav-button" onClick={handleClick}> */}
+          <FavBadge isFavPhotoExist={isFavPhotoExist} />
+        {/* </button> */}
       
     </div>
   )
