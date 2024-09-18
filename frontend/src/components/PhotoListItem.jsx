2bc8eef92f4d8b74b9d3a1403ab489682d70d7c4
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-17 19:06:52 -0600

    Modify code to have useState hook to open and close blank modal

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   /* Insert React */
 
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className="photo-list__item">
       <PhotoFavButton globalFavorite={props.globalFavorite} id={props.id} />
-      <img className="photo-list__image" src={props.imageSource}/>      
+      <img className="photo-list__image" src={props.imageSource} onClick={openModal}/>      
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.profile} />
         <div className="photo-list__user-info">
@@ -17,7 +22,10 @@ const PhotoListItem = (props) => {
           <p className="photo-list__user-location">{props.city}, {props.country}</p>
         </div>
       </div>
-      
+      {
+        isModalOpen && 
+        <PhotoDetailsModal closeModal={closeModal}/>
+      }
     </div>
   )
 };
