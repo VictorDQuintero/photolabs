097febb9821621ab353bc1d517442dea8a634a22
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-26 16:23:40 -0600

    Modify code to send better props to children components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,32 +7,61 @@ import PhotoDetailsModal from './PhotoDetailsModal';
 
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = (props) => {
+// const HomeRoute = (props) => {
   
+//   return (
+//     <div className="home-route">
+//       {/* Insert React */}
+//       <TopNavigation 
+//         topics={props.topics} 
+//         favoritePhotos={props.favoritePhotos} 
+//       />
+//       <PhotoList 
+//         photos={props.photos} 
+//         globalFavorite={props.globalFavorite}
+//         displayModal={props.displayModal}
+//         isModalOpen={props.isModalOpen}
+//       />
+//       {   
+//         props.isModalOpen 
+//         && 
+//         <PhotoDetailsModal 
+//           photos={props.photos}
+//           closeModal={props.displayModal} 
+//           isModalOpen={props.isModalOpen} 
+//           singlePhotoDetail={props.singlePhotoDetail}
+//         />
+       
+//       }      
+//     </div>
+//   );
+// };
+
+const HomeRoute = ({ photos, topics, globalFavorite, favoritePhotos, displayModal, isModalOpen, singlePhotoDetail, closeModal }) => {
+  console.log("HomeRoute photos:", photos); // Debugging line
   return (
     <div className="home-route">
-      {/* Insert React */}
+      {/* Render topics and other sections as needed */}
       <TopNavigation 
-        topics={props.topics} 
-        favoritePhotos={props.favoritePhotos} 
+        topics={topics} 
+        favoritePhotos={favoritePhotos} 
+       />
+      <PhotoList
+        photos={photos}
+        globalFavorite={globalFavorite}
+        favoritePhotos={favoritePhotos}
+        displayModal={displayModal}
+        isModalOpen={isModalOpen}
+        singlePhotoDetail={singlePhotoDetail}
       />
-      <PhotoList 
-        photos={props.photos} 
-        globalFavorite={props.globalFavorite}
-        // openModal={props.openModal}
-        displayModal={props.displayModal}
-        isModalOpen={props.isModalOpen}
-      />
-      {   
-        props.isModalOpen 
-        && 
-        <PhotoDetailsModal 
-          closeModal={props.displayModal} 
-          isModalOpen={props.isModalOpen} 
-          singlePhotoDetail={props.singlePhotoDetail}
+      {isModalOpen && singlePhotoDetail && (
+        <PhotoDetailsModal
+          closeModal={closeModal}
+          singlePhotoDetail={singlePhotoDetail}
+          globalFavorite={globalFavorite}
+          photos={singlePhotoDetail.similar_photos}
         />
-        //used to be closeModal={props.closeModal}
-      }      
+      )}
     </div>
   );
 };
