53312105fe7580809b580660ed3c170ca773023a
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-27 17:20:47 -0600

    Remove comments

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,24 +1,11 @@
 //TODO fix button, because the heart icon is askew
 // might want to consider checking if the photo ID exists in the global favorites array and updating the local state accordingly. This would ensure that the favorite status of a photo is consistent across the application.
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-
-  // const isFavorite =  props.favoritePhotos.find(idItem => idItem === props.id);
-
-  
-
-  // const [ favorited, setFavorited ] = useState(isFavorite ? true : false);
-
-  // const handleClick = () => {
-    
-  //   setFavorited(!favorited)
-  // };  
-
-
 
   return (
     <div className="photo-list__fav-icon">
