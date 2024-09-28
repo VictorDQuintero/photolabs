fc0beee7dc1b8b39e2cf08240282c2a7607eabc8
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-27 18:40:16 -0600

    Format code, remove commented out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,62 +19,13 @@ const App = () => {
 
   const preparedPhotos = prepareData(photos);
   const preparedTopics = prepareData(topics);
-  const { updateToFavPhotos, favoritePhotos, setPhotoSelected, isModalOpen, singlePhotoDetail, onClosePhotoDetailsModal } = useApplicationData();
-
-  // const [ globalFavId, setGlobalFavId ] = useState(null);
-  // const [ favoritePhotos, setFavoritePhotos ] = useState([]);
-  // const [ isModalOpen, setIsModalOpen ] = useState(false);
-  // const [ singlePhotoDetail, setSinglePhotoDetail ] = useState({})
-  
-  // const addFavorite = (id) => {
-  //   setFavoritePhotos(prevPhotos => [...prevPhotos, id]) // puts the fav photo id's in array
-  // }
-
-  // const removeFavorite = (id) => {
-  //   setFavoritePhotos( prevPhotos => prevPhotos.filter(idItem => idItem !== id));// removes the photo id
-  // }
-
-  // const handleClickFav = (id) => {
-
-  //   setGlobalFavId(id); // sets the photo id  
-
-  //   favoritePhotos.includes(id) ? removeFavorite(id) : addFavorite(id);
-
-  // }
-
-// const openModal = (photoId) => {
-//   const photoDetails = preparedPhotos.find(photo => photo.id === photoId) || {};
-//   let similarPhotosArray = photoDetails.similar_photos || [];
-
-//   //make use of prepareddata function
-
-//   if (typeof similarPhotosArray === "object" && !Array.isArray(similarPhotosArray)) {
-//     similarPhotosArray = Object.values(similarPhotosArray);
-//   }
-
-//   console.log("Opening Modal with photoDetails:", photoDetails);
-//   console.log("Similar Photos:", similarPhotosArray);
-
-//   if (!singlePhotoDetail || singlePhotoDetail.id !== photoId) {
-//     setSinglePhotoDetail({
-//       ...photoDetails,
-//       similar_photos: similarPhotosArray
-//     });
-//     setIsModalOpen(true);
-//   }
-
-//   setSinglePhotoDetail({
-//     ...photoDetails,
-//     similar_photos: similarPhotosArray
-//   });
-//   setIsModalOpen(true);
-// };
-
-// const closeModal = () => {
-//   setIsModalOpen(false);
-//   setSinglePhotoDetail({similar_photos: []}); // Reset to initial state
-// };
-
+  const { updateToFavPhotos, 
+          favoritePhotos, 
+          setPhotoSelected,   
+          isModalOpen, 
+          singlePhotoDetail, 
+          onClosePhotoDetailsModal 
+        } = useApplicationData();
 
 return (
   <div className="App">
