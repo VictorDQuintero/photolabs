6dcfcb8b8cfcfa8f0a5744c87c9d3845055592ee
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-29 22:36:22 -0600

    Modify JSX structure

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -13,33 +13,49 @@ const PhotoDetailsModal = ({ closeModal, singlePhotoDetail, toggleFavorite, phot
   const isFavorite = favoritePhotos.includes(singlePhotoDetail.id);
 
   return (
-    <div className="photo-details-modal">
-      <div className="photo-details-modal__top-bar">
-        <button className="photo-details-modal__close-button" onClick={closeModal}>
+      <div className="photo-details-modal">
+        <div className="photo-details-modal__top-bar">
+          <button className="photo-details-modal__close-button" onClick={closeModal}>
           <img src={closeSymbol} alt="close symbol" />
-        </button>
-      </div>
-      <PhotoFavButton onClick={() => toggleFavorite(singlePhotoDetail.id)}  isFavorite={isFavorite} />
+          </button>
+         </div>
       <div className="photo-details-modal__images">
-        <img className="photo-details-modal_image" src={singlePhotoDetail.urls.full} alt={singlePhotoDetail.description} />     
+        <div className="photo-details-modal__favorite-button">
+            <PhotoFavButton
+              onClick={() => toggleFavorite(singlePhotoDetail.id)}
+              isFavorite={isFavorite}
+            />
+          </div>
+          <img
+          className="photo-details-modal__image"
+          src={singlePhotoDetail.urls.full}
+          alt={singlePhotoDetail.description}
+        />
+        
         <div className="photo-details-modal__photographer-details">
-          <img src={singlePhotoDetail.user.profile} alt="" className="photo-details-modal__photographer-profile" />
+          <img
+            src={singlePhotoDetail.user.profile}
+            alt=""
+            className="photo-details-modal__photographer-profile"
+          />
           <div className="photo-details-modal__photographer-info">
             <p>{singlePhotoDetail.user.name}</p>
-            <p className="photo-details-modal__photographer-location">{singlePhotoDetail.location.city} {singlePhotoDetail.location.country}</p>
+            <p className="photo-details-modal__photographer-location">
+              {singlePhotoDetail.location.city} {singlePhotoDetail.location.country}
+            </p>
           </div>
         </div>
-        <div className="photo-details-modal__header">
-          Similar Photos
-        </div>      
-        <div className="photo-details-modal__images">
-          <PhotoList
-            photos={similarPhotos}
-            singlePhotoDetail={singlePhotoDetail}            
-            toggleFavorite={toggleFavorite}
-            favoritePhotos={favoritePhotos}
-          />
-        </div>
+        <div className="photo-details-modal__header">Similar Photos</div>
+          <div className="photo-details-modal__images">
+            <div  className="photo-details-modal__similar-photos">
+              <PhotoList
+                photos={similarPhotos}
+                singlePhotoDetail={singlePhotoDetail}
+                toggleFavorite={toggleFavorite}
+                favoritePhotos={favoritePhotos}
+              />
+            </div>
+          </div>
       </div>
     </div>
   );
