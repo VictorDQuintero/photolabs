3c2ddcf35638fbc565da90d281b5a91fefceea4d
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-30 17:04:27 -0600

    Modify props and add contional rendering for topic pictures

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,68 +4,8 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-
-// const PhotoList = (props) => {
-
-//   const photos = props.photos.map((photo) => {
-
-//     return (
-//     <li key={photo.id}>
-//       <PhotoListItem 
-//       imageSource={photo.urls.regular}
-//       profile={photo.user.profile}
-//       name={photo.user.name}
-//       city={photo.location.city}
-//       country={photo.location.country}
-//       id={photo.id} // send id to identify potentially favourited picture
-//       globalFavorite={props.globalFavorite}
-//       // openModal={props.openModal}
-//       displayModal={props.displayModal}
-//       isModalOpen={props.isModalOpen}
-//       />
-//     </li>
-//     );
-//   });
-  
-//     const similarPhotos = props.isModalOpen && props.singlePhotoDetails ? (
-//       props.singlePhotoDetails.similar_photos.map((similarPhoto) => (
-        
-//         <li key={similarPhoto.id}>
-//           <PhotoListItem
-//             imageSource={similarPhoto.urls.regular}
-//             profile={similarPhoto.user.profile}
-//             name={similarPhoto.user.name}
-//             city={similarPhoto.location?.city}
-//             country={similarPhoto.location?.country}
-//             id={similarPhoto.id}
-//             globalFavorite={props.globalFavorite}
-//             isModalOpen={true}  // This is inside the modal
-//           />
-//         </li>
-//       ))
-//     ) : null;
-
-//   return (
-// <div>
-//     <ul className="photo-list">
-//       {/* Insert React */}
-      
-
-//       { photos}
-//     </ul>
-//     {props.isModalOpen && (
-//         <ul className="photo-list">
-//           {similarPhotos}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// };
-
-const PhotoList = ({ photos, singlePhotoDetail, displayModal, isModalOpen, toggleFavorite: toggleFavorite, favoritePhotos }) => {
-  console.log("In photoList: ", favoritePhotos); // Debugging line  
-
-  
+const PhotoList = ({ photos, singlePhotoDetail, displayModal, isModalOpen, toggleFavorite, favoritePhotos }) => {
+   
   const photoItems = photos.map(photo => (
     <li key={photo.id}>
       <PhotoListItem
@@ -89,8 +29,8 @@ const PhotoList = ({ photos, singlePhotoDetail, displayModal, isModalOpen, toggl
             imageSource={similarPhoto.urls.regular}
             profile={similarPhoto.user.profile}
             name={similarPhoto.user.name}
-            city={similarPhoto.location?.city}
-            country={similarPhoto.location?.country}
+            city={similarPhoto.location.city}
+            country={similarPhoto.location.country}
             id={similarPhoto.id}
             toggleFavorite={toggleFavorite}
             favoritePhotos={favoritePhotos}
