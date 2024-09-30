c75a78a27ee79c5bd0f17a384afc0a229bbd47ab
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-30 17:06:20 -0600

    Modify props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,18 +1,17 @@
-//TODO Check with mentor if there's another way of styling the button other than making a new style. Check the style in TopNavigationBar.scss
-//TODO the favbdge does not need to be a button, at least not now
-
-import React, { useState } from 'react';
+import React from 'react';
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = ({ topics, handleTopicClick, favoritePhotos}) => {
+const TopNavigation = ({ topics, handleTopicClick, favoritePhotos, handleLogoClick}) => {
 
 const isFavPhotoExist = favoritePhotos.length > 0;
 
  return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <button onClick={handleLogoClick}>
+        <span className="top-nav-bar__logo">PhotoLabs</span>
+      </button>
       <TopicList topics={topics} handleTopicClick={handleTopicClick} />
           <FavBadge isFavPhotoExist={isFavPhotoExist} />             
     </div>
