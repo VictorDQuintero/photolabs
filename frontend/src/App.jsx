8d1f8a7cd6658e1653c6744299677b2c69840a40
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-30 19:24:45 -0600

    Remove comments, unnecessary functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,10 @@
-// make use of helper function in singlePhotoDetail state
-
-import React, { useState } from 'react';
+import React from 'react';
 
 import HomeRoute from 'routes/HomeRoute';
 import useApplicationData from 'hooks/useApplicationData';
 
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
-
 import './App.scss';
 
-const prepareData = function (object) {
-    return Object.values(object);
-  }
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
 
   const { 
@@ -26,25 +16,23 @@ const App = () => {
           handleLogoClick
         } = useApplicationData();
 
-      console.log("This is photos by topic: ", state.photosByTopic)
-
-return (
-  <div className="App">
-    <HomeRoute
-      photos={state.photoData}
-      topics={state.topicData}
-      photosByTopic={state.photosByTopic}
-      handleTopicClick={handleTopicClick}
-      handleLogoClick={handleLogoClick}
-      toggleFavorite={updateToFavPhotos}
-      favoritePhotos={state.favoritePhotos}
-      displayModal={setPhotoSelected}
-      isModalOpen={state.isPhotoSelected}
-      singlePhotoDetail={state.singlePhotoDetail}
-      closeModal={onClosePhotoDetailsModal}
-    />
-  </div>
-);
+  return (
+    <div className="App">
+      <HomeRoute
+        photos={state.photoData}
+        topics={state.topicData}
+        photosByTopic={state.photosByTopic}
+        handleTopicClick={handleTopicClick}
+        handleLogoClick={handleLogoClick}
+        toggleFavorite={updateToFavPhotos}
+        favoritePhotos={state.favoritePhotos}
+        displayModal={setPhotoSelected}
+        isModalOpen={state.isPhotoSelected}
+        singlePhotoDetail={state.singlePhotoDetail}
+        closeModal={onClosePhotoDetailsModal}
+      />
+    </div>
+  );
 };
 
 export default App;
