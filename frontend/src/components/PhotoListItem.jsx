d4e74116dca5d4b699af4dba90556c1d27b23497
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-30 18:32:57 -0600

    Remove comments, unused imports, destructure props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import PhotoFavButton from 'components/PhotoFavButton';
-import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = (props) => {
+const PhotoListItem = ({ favoritePhotos, id, isModalOpen, displayModal, toggleFavorite, imageSource, profile, name, city, country }) => {
  
-  const isFavorite = props.favoritePhotos.includes(props.id);
+  const isFavorite = favoritePhotos.includes(id);
 
   return (
     <div className="photo-list__item">
-      <PhotoFavButton onClick={() => props.toggleFavorite(props.id)}  isFavorite={isFavorite} />
-      <img className="photo-list__image" src={props.imageSource}  onClick={()=> props.displayModal(props.isModalOpen, props.id)}/>      
+      <PhotoFavButton onClick={() => toggleFavorite(id)}  isFavorite={isFavorite} />
+      <img className="photo-list__image" src={imageSource}  onClick={()=> displayModal(isModalOpen, id)}/>      
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.profile} />
+        <img className="photo-list__user-profile" src={profile} />
         <div className="photo-list__user-info">
-          <p>{props.name}</p>
-          <p className="photo-list__user-location">{props.city}, {props.country}</p>
+          <p>{name}</p>
+          <p className="photo-list__user-location">{city}, {country}</p>
         </div>
       </div>  
     </div>
