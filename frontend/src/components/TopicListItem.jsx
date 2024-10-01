5453a444b7f780fb7ac151e98edece62380e8850
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-09-30 19:25:12 -0600

    Remove prop

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({ label, slug, onClick}) => {
+const TopicListItem = ({ label, onClick}) => {
 
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
       <button onClick={onClick}>
         <span className="topic-list__item span">{label}</span>
       </button>
